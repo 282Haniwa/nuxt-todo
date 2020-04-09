7b0bcf9f637ec996841c4fa9a99b0254e48a5197
Kazuma Usui <k_usui@yumemi.co.jp>
2020-04-09 14:52:52 +0900

    add: Add svg loader.

---
+++ src/components/SvgImage/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="svg" />
+</template>
+
+<script>
+export default {
+  name: 'SvgImage',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    svg() {
+      return () => import(`~/assets/img/${this.name}.svg`);
+    },
+  },
+};
+</script>
