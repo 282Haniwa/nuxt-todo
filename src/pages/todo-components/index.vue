906edddb2f202abb2351b2713d222cb9eb90ede1
Kazuma Usui <k_usui@yumemi.co.jp>
2020-04-10 10:54:48 +0900

    feat: テキストエリアコンポーネントの作成

---
+++ src/pages/todo-components/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>Component list</div>
+    <hr width="100%" />
+    <span>TextArea</span>
+    <TextArea label="ラベル" />
+    <hr width="100%" />
+  </div>
+</template>
+
+<script>
+import TextArea from '~/components/Inputs/TextArea';
+
+export default {
+  components: { TextArea },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 300px;
+
+  & > * {
+    margin-bottom: 32px;
+  }
+}
+</style>
