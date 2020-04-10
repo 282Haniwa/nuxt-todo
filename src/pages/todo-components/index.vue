8eccb781a3b37de9f11e8f2fe9bfe60c9b64a78e
Kazuma Usui <k_usui@yumemi.co.jp>
2020-04-10 12:42:10 +0900

    feat: ボタンコンポーネントの作成

---
+++ src/pages/todo-components/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div>Component list</div>
+    <hr width="100%" />
+    <span>Button</span>
+    <Button variant="main">Button</Button>
+    <Button variant="lime">Button</Button>
+    <Button variant="white">Button</Button>
+    <Button variant="outline">Button</Button>
+    <Button icon="icon/edit" variant="main">Button</Button>
+    <hr width="100%" />
+  </div>
+</template>
+
+<script>
+import Button from '~/components/Buttons/Button';
+
+export default {
+  components: { Button },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 300px;
+
+  & > * {
+    margin-bottom: 32px;
+  }
+}
+</style>
