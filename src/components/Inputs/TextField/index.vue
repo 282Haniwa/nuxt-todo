efa627c22b137c6a8369cce0409b1f5614b575cb
Kazuma Usui <k_usui@yumemi.co.jp>
2020-04-10 09:43:07 +0900

    feat: テキストフィールドコンポーネントの作成

---
+++ src/components/Inputs/TextField/index.vue
@@ -0,0 +1,72 @@
+<docs>
+# テキスト入力フォーム
+</docs>
+
+<template>
+  <div class="root">
+    <div v-if="label" class="label-wrapper">
+      <label class="label">{{ label }}</label>
+    </div>
+    <input
+      :value="value"
+      class="text-field"
+      type="text"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, null],
+      default: '',
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.root {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 120px;
+}
+
+.label-wrapper {
+  width: 100%;
+}
+
+.label {
+  font-family: Sawarabi Gothic;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 21px;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.05em;
+  color: $color-text;
+}
+
+.text-field {
+  width: 100%;
+  border: 1px solid $color-border;
+  box-sizing: border-box;
+  border-radius: 5px;
+  font-family: Sawarabi Gothic;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  color: $color-text;
+  line-height: 21px;
+  letter-spacing: 0.05em;
+  padding: 6px 11px 5px 11px;
+  color: inherit;
+}
+</style>
---
+++ src/pages/todo-components/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>Component list</div>
+    <hr width="100%" />
+    <span>TextField</span>
+    <TextField label="ラベル" />
+    <hr width="100%" />
+  </div>
+</template>
+
+<script>
+import TextField from '~/components/Inputs/TextField';
+
+export default {
+  components: { TextField },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 300px;
+
+  & > * {
+    margin-bottom: 32px;
+  }
+}
+</style>
